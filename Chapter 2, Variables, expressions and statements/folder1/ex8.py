3d964d4fd5e323e8e73d94e26566979ade18bd66
spidy.G <120951553+JayKalt@users.noreply.github.com>
2024-09-11 12:16:00 +0200

    Sorry my bad

--- Chapter 2, Variables, expressions and statements/folder1/ex8.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to solve the general version of the above problem.
-# Ask the user for the time now (in hours), and ask for the number of hours to wait.
-# Your program should output what the time will be on the clock when the alarm goes off.
-
-current_time = int(input("What time is it? "))
-to_wait = int(input("How many hours before the alarm goes off? "))
-
-print("The alarm goes off at", (current_time + to_wait) % 24)
